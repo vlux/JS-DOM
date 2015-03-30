c40a6c1dd8c7aeeb165d4fd937413058d08e6f45
Jmcus <vluxyue@gmail.com>
2015-03-30 19:55:56 +0800

    ajax

---
+++ getNewContent.js
@@ -0,0 +1,18 @@
+function getNewContent(){
+    var request = getHTTPObject();
+    if(request){
+        request.open('GET','example.txt',true);
+        request.onreadystatechange = function(){
+            if(request.readyState == 4){    //0:未初始化 1:正在加载 2:加载完毕 3:正在交互 4:完成
+                var para = document.createElement('p');
+                var txt = document.createTextNode('request.responseText');
+                para.appendChild(txt);
+                document.getElementById('new').appendChild(para);
+            }
+        };
+        request.send(null);
+    }else{
+        alert('Sorry,ur browser doesn\'t support XMLHttpRequest');
+    }
+}
+addLoadEvent(getNewContent);
